b1dd4e1d1fa5ff7d265114f562edde0d899c9308
Sarah Situ <sarahsitu563@gmail.com>
2024-07-14 17:21:53 +1200

    Add Nav bar in header

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,10 +1,11 @@
-
+import Header from "./Header"
 function App() {
 
 
   return (
     <>
       <div className="app">
+        <Header/>
         <h1>Portfolio</h1>
       </div>
     </>
---
+++ client/components/Header.tsx
@@ -0,0 +1,17 @@
+
+export default function Header(){
+    return (
+        <header>
+            <nav><h1>
+            Sarah Situ
+        </h1>
+                <ul>
+                  <li><a href="#home" className="nav-link">Home</a></li>
+                  <li><a href="#about" className="nav-link">About</a></li>
+                  <li><a href="#projects" className="nav-link">Projects</a></li>
+                  <li><a href="#Contact" className="nav-link">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+} 
