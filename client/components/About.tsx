093507365dccbd762586f0435eaa376ec470bc0d
Sarah Situ <sarahsitu563@gmail.com>
2024-07-15 00:10:01 +1200

    Center about info

--- client/components/About.tsx
+++ client/components/About.tsx
@@ -1,15 +1,20 @@
 export default function Home() {
   return (
-    <>
+    <><div className="about-me">
       <h1>Kia Ora! My name is Sarah</h1>
-      <p className="bio-text">
-        Welcome to my portfolio. I&apos;m a web developer based in Wellington,
-        New Zealand.</p>
-        <p className="bio-text">As a recent career changer, I&apos;ve recently finished up
-        an intensive web development program at Deve Academy Aotearor with
-        JavaScript, React and TypeScript.</p><p className="bio-text">I&apos;m excited to put my skills to
-        work and create impactful projects and continue to grow as a developer.
-      </p>
+      
+        <p className="bio-text">
+          Welcome to my portfolio. I&apos;m a web developer based in Wellington,
+          New Zealand.
+          <br></br>
+          As a recent career changer, I&apos;ve recently finished up an
+          intensive web development program at Deve Academy Aotearor with
+          JavaScript, React and TypeScript.
+          <br></br>
+          I&apos;m excited to put my skills to work and create impactful
+          projects and continue to grow as a developer.
+        </p>
+      </div>
     </>
   )
 }
