cd8eac39165f5db3bbf7cfdfa4da86f32b4d087c
Sarah Situ <sarahsitu563@gmail.com>
2024-07-14 16:21:35 +1200

    Remove fruits from App

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,13 +1,11 @@
-import { useFruits } from '../hooks/useFruits.ts'
 
 function App() {
-  const { data } = useFruits()
+
 
   return (
     <>
       <div className="app">
-        <h1>Fullstack Boilerplate - with Fruits!</h1>
-        <ul>{data && data.map((fruit) => <li key={fruit}>{fruit}</li>)}</ul>
+        <h1>Portfolio</h1>
       </div>
     </>
   )
